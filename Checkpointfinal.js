390bc8bbd19ddd4698b936ef96872b6cff3266a8
MauroM13 <mauroluciomartins@gmail.com>
2021-09-30 20:21:56 -0300

    Checkpoint 3 JS

---
+++ Checkpointfinal.js
@@ -0,0 +1,19 @@
+
+	let nomeAluno = Mauro;
+    let notasMauro = [19, 15, 8];
+    let faltas = 3;
+
+	function Aluno(nomeAluno, faltas, notas) {
+        this.nome = nomeAluno;
+        this.quantidadeFaltas = faltas;
+        this.notas = notasMauro;
+        
+        //nomeDoObjeto.nomedometodo = nome_da_funcao;
+
+        this.quantidadeFaltas = function (amentarFaltas)  {
+          
+
+         }
+       
+    
+	
